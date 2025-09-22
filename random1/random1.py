42272f238dc9c4270882c2b120cec747738981ba
tahakartal06 <tahameliksah@hotmail.com>
2025-09-22 19:53:18 +0300

    deneme

---
+++ random1/random1.py
@@ -0,0 +1,6 @@
+import random
+# print(random.random())
+# print(random.randint(0,100))
+# # print(random.choice(["Yazı","Tura"]))
+# secenekler = ["Yazı","Tura"]
+# print(random.choice(secenekler))
